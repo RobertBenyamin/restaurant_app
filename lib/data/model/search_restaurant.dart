e86123cf4a79d8eb19fad87962b5d9aa7cf6d711
RobertBenyamin <robertbenyamino@gmail.com>
2023-12-20 23:32:49 +0700

    change: move Restaurant class to a new file

--- lib/data/model/search_restaurant.dart
+++ lib/data/model/search_restaurant.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'restaurant.dart';
 
 RestaurantSearch restaurantSearchFromJson(String str) => RestaurantSearch.fromJson(json.decode(str));
 
@@ -25,41 +26,5 @@ class RestaurantSearch {
         "error": error,
         "founded": founded,
         "restaurants": List<dynamic>.from(restaurants.map((x) => x.toJson())),
-    };
-}
-
-class Restaurant {
-    String id;
-    String name;
-    String description;
-    String pictureId;
-    String city;
-    double rating;
-
-    Restaurant({
-        required this.id,
-        required this.name,
-        required this.description,
-        required this.pictureId,
-        required this.city,
-        required this.rating,
-    });
-
-    factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
-        id: json["id"],
-        name: json["name"],
-        description: json["description"],
-        pictureId: json["pictureId"],
-        city: json["city"],
-        rating: json["rating"]?.toDouble(),
-    );
-
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "description": description,
-        "pictureId": pictureId,
-        "city": city,
-        "rating": rating,
     };
 }
