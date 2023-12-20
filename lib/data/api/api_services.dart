897a26d910f4546e60b5b3fe3c8d035401b4a8d6
RobertBenyamin <robertbenyamino@gmail.com>
2023-12-20 20:55:15 +0700

    change: url visibility to public

--- lib/data/api/api_services.dart
+++ lib/data/api/api_services.dart
@@ -7,11 +7,11 @@ import 'package:restaurant_app/data/model/list_restaurant.dart';
 import 'package:restaurant_app/data/model/search_restaurant.dart';
 
 class ApiServices {
-  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
-  static const String _imageUrl = 'https://restaurant-api.dicoding.dev/images/large/';
+  static const String baseUrl = 'https://restaurant-api.dicoding.dev/';
+  static const String imageUrl = 'https://restaurant-api.dicoding.dev/images/large/';
 
   Future<RestaurantList> getRestaurantList() async {
-    final response = await http.get(Uri.parse('${_baseUrl}list'));
+    final response = await http.get(Uri.parse('${baseUrl}list'));
     if (response.statusCode == 200) {
       return RestaurantList.fromJson(json.decode(response.body));
     } else {
@@ -20,7 +20,7 @@ class ApiServices {
   }
 
   Future<RestaurantDetail> getRestaurantDetail(id) async {
-    final response = await http.get(Uri.parse('${_baseUrl}detail/$id'));
+    final response = await http.get(Uri.parse('${baseUrl}detail/$id'));
     if (response.statusCode == 200) {
       return RestaurantDetail.fromJson(json.decode(response.body));
     } else {
@@ -29,7 +29,7 @@ class ApiServices {
   }
 
   Future<RestaurantSearch> getRestaurantSearch(query) async {
-    final response = await http.get(Uri.parse('${_baseUrl}search?q=$query'));
+    final response = await http.get(Uri.parse('${baseUrl}search?q=$query'));
     if (response.statusCode == 200) {
       return RestaurantSearch.fromJson(json.decode(response.body));
     } else {
