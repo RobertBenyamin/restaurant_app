2cdead8919e0508117e492fb1abed1045238e73e
RobertBenyamin <robertbenyamino@gmail.com>
2023-12-20 17:48:23 +0700

    feat: create models and api services

---
+++ lib/data/api/api_services.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: unused_field
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:restaurant_app/data/model/detail_restaurant.dart';
+import 'package:restaurant_app/data/model/list_restaurant.dart';
+import 'package:restaurant_app/data/model/search_restaurant.dart';
+
+class ApiServices {
+  static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
+  static const String _imageUrl = 'https://restaurant-api.dicoding.dev/images/large/';
+
+  Future<RestaurantList> getRestaurantList() async {
+    final response = await http.get(Uri.parse('${_baseUrl}list'));
+    if (response.statusCode == 200) {
+      return RestaurantList.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant list');
+    }
+  }
+
+  Future<RestaurantDetail> getRestaurantDetail(id) async {
+    final response = await http.get(Uri.parse('${_baseUrl}detail/$id'));
+    if (response.statusCode == 200) {
+      return RestaurantDetail.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant detail');
+    }
+  }
+
+  Future<RestaurantSearch> searchRestaurant(query) async {
+    final response = await http.get(Uri.parse('${_baseUrl}search?q=$query'));
+    if (response.statusCode == 200) {
+      return RestaurantSearch.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load restaurant list');
+    }
+  }
+}
